448c517cf130489798c9244b251425c2a57ae2dd
LevitateGamer <LevitateGamer@gmail.com>
2020-11-01 11:17:26 -0800

    Add files via upload

---
+++ Compile.bat
@@ -0,0 +1,11 @@
+nasm src/bootloader.asm -f bin -i src -o bootloader.bin
+
+nasm src/ExtendedProgram.asm -f elf64 -i src -o ExtendedProgram.o
+
+cmake CMakeLists.txt -G "Unix Makefiles" -D CMAKE_CXX_COMPILER=x86_64-elf-gcc -D CMAKE_C_COMPILER=x86_64-elf-gcc
+
+make -f "Makefile"
+
+copy /b bootloader.bin+kernel.bin bootloader.flp
+
+pause
